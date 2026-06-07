55e0785536645b800b1fdcf418cc75d4650a759b
vbaniRC <robert@gkaura.com>
2026-06-08 01:09:15 +0200

    Restore (auth) route group

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }) {
   return (
-    <div className="min-h-screen bg-neutral-950 flex items-center justify-center">
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
       {children}
     </div>
   );
