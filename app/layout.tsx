499bde65788e8828d5c5d42893202061c09c644c
vbaniRC <robert@gkaura.com>
2026-06-08 19:56:22 +0200

    Increase max width of auth container to 460px

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       <body className="text-white font-sans">
         {isAuth ? (
           <div className="min-h-screen w-full flex items-center justify-center bg-black px-4">
-            <div className="w-full max-w-[420px] mx-auto">
+            <div className="w-full max-w-[460px] mx-auto">
               {children}
             </div>
           </div>
