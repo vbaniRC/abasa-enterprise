c9c0e3d1011a1ec738b3584572c05719b0c0d95e
vbaniRC <robert@gkaura.com>
2026-06-11 12:51:41 +0200

    Enable experimental server actions in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
   },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
+
