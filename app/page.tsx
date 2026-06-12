48f2b018695dcb63c7dff827a6a1b5dc3a11a430
vbaniRC <robert@gkaura.com>
2026-06-12 15:50:07 +0000

    Tailwind test after CSS pipeline fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@
 
 export default function Page() {
   return (
-    <main className="relative min-h-screen w-full">
-      <div className="absolute top-10 left-10 w-40 h-40 bg-red-500"></div>
-      <h1 className="text-5xl font-bold text-blue-500">Test</h1>
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <div className="w-40 h-40 bg-red-500"></div>
     </main>
   );
 }
