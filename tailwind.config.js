5c4aa0069c0c4ee466e8ad7d8be8c2f2527e5159
vbaniRC <robert@gkaura.com>
2026-06-12 14:19:52 +0000

    Fix Tailwind background image via theme.extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "abasa-hero":
+          "url('https://grvomwpejsgokxcandkr.supabase.co/storage/v1/object/public/public-assets/landing_page.png')",
+      },
+    },
   },
   plugins: [],
 };
